1a68420ac6b756e11163d2e267d845aa92306d30
hausfrau <hausfrau.sunlight@gmail.com>
2019-03-14 07:42:56 +0300

    feat: complete Love triangles task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,40 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    const obj = {};
+
+    const isUnique = function (arr) {
+        let result = true;
+        arr.forEach(function (element, index, array) {
+            if (array[index] === array[index + 1]) {
+                result = false;
+            }
+        });
+        return result;
+    };
+
+    const getLovers = function (index) {
+        let arr = [];
+        arr.push(index);
+        let currentIndex = preferences[index - 1];
+        while (arr.length <= 3) {
+            arr.push(currentIndex);
+            currentIndex = preferences[currentIndex - 1];
+        }
+
+        if (arr[0] === arr[3]) {
+            arr = arr.slice(0, 3).sort();
+            if (isUnique(arr)) {
+                obj[arr] = true;
+            }
+        }
+    };
+
+    preferences.forEach(
+        function (element) {
+            getLovers(element);
+        }
+    );
+
+    return Object.keys(obj).length;
 };
